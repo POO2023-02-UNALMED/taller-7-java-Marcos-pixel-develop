b0eb977fa0391392e9e279875e21876861273682
Marcos-pixel-develop <73051397+Marcos-pixel-develop@users.noreply.github.com>
2023-10-09 23:46:36 -0500

    Update Taller7.java

--- src/main/java/comunicacion/Taller7.java
+++ src/main/java/comunicacion/Taller7.java
@@ -1,9 +1,1 @@
-package comunicacion;
 
-public class Taller7 {
-	public static void main(String[] args) {
-		Libro libro = new Libro("3","2","5","sad");
-		System.out.print(libro.toString());
-	}
-
-}
